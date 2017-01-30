b2fb1ecc265f6ff46126bad32af37a459bb42a1d
NicolasDorier <nicolas.dorier@gmail.com>
2017-01-31 02:36:54 +0900

    fix pushnuget

--- NBitcoin.TestFramework/PushNuget.ps1
+++ NBitcoin.TestFramework/PushNuget.ps1
@@ -1,15 +1,15 @@
-del "*.nupkg"
-
 dotnet restore
-dotnet build -c Release
+dotnet pack -c Release
 
-msbuild.exe "PushNuget.csproj" /p:Configuration=Release
-
-nuget pack "../QBitNinja.Client/QBitNinja.Client.nuspec"
-nuget pack "../QBitNinja/QBitNinja.nuspec"
+cd bin/Release
 
 forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE -source https://api.nuget.org/v3/index.json"
+
+del "*symbols.nupkg"
 (((dir *.nupkg).Name)[0] -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
 $ver = $Matches.Item(0)
 git tag -a "v$ver" -m "$ver"
 git push --tags
+del "*.nupkg"
+
+cd ../..
