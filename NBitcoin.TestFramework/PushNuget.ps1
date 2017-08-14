cd2628b18c26a90b9f970f789ce2d0b9ae440d86
NicolasDorier <nicolas.dorier@gmail.com>
2017-08-14 10:51:03 +0900

    update pushnuget

--- NBitcoin.TestFramework/PushNuget.ps1
+++ NBitcoin.TestFramework/PushNuget.ps1
@@ -1,15 +1,6 @@
-dotnet restore
-dotnet pack -c Release
-
-cd bin/Release
-
-forfiles /m *.nupkg /c "cmd /c NuGet.exe push @FILE -source https://api.nuget.org/v3/index.json"
-
-del "*symbols.nupkg"
-(((dir *.nupkg).Name)[0] -match "[0-9]+?\.[0-9]+?\.[0-9]+?\.[0-9]+")
-$ver = $Matches.Item(0)
+rm "bin\release\" -Recurse -Force
+dotnet pack --configuration Release
+dotnet nuget push "bin\Release\" --source "https://api.nuget.org/v3/index.json"
+$ver = ((ls .\bin\release\*.nupkg)[0].Name -replace '[^\.]*\.(\d+(\.\d+){1,3}).*', '$1')
 git tag -a "v$ver" -m "$ver"
 git push --tags
-del "*.nupkg"
-
-cd ../..
